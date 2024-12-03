e4f65ed2ad13049155c74eda382ef6542650759d
nishantyaji <nishant.yaji@gmail.com>
2024-12-03 23:40:08 +0530

    Solution to CSES Binomial Co-efficients

--- BinomialCoefficients.py
+++ BinomialCoefficients.py
@@ -4,19 +4,11 @@ num_test = int(input().strip())
 for _ in range(num_test):
     [a, b] = list(map(int, input().strip().split()))
     x = min(a - b, b)
-
-    cntr = {}
-    for i in range(2, x + 1):
-        r = a % i
-        if (a - r) not in cntr:
-            cntr[a - r] = i
-        else:
-            cntr[a - r] = cntr[a - r] * i
-
     res = 1
-    for bx in range(1, x + 1):
-        res *= (a + 1 - bx)
-        if (a + 1 - bx) in cntr:
-            res //= cntr[a + 1 - bx]
-        res = (res % base)
+    num = 1
+    den = 1
+    for i in range(1, x + 1):
+        num = (num * (a + 1 - i)) % base
+        den = (den * i) % base
+    res = (num * pow(den, -1, base)) % base
     print(res)
